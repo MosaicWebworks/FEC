e15d0f6fe1e20fff442c4017b60cb6f0f1c1da65
luckynumberthirteen <jtobono@gmail.com>
2023-08-09 16:00:50 -0700

    Got webpack and babel up and running

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "client/src");
+var DIST_DIR = path.resolve(__dirname, "client/dist");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: "development",
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
