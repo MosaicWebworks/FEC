dea32181747c5b6a0224a3d3ebdfd1c40bce8983
luckynumberthirteen <jtobono@gmail.com>
2023-08-10 15:59:34 -0700

    Resolved conflicts

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import QuestionList from './components/qa/QuestionList.jsx'
-
+import { Overview } from './components/Overview/Overview.jsx'
 const App = () => {
 
   return (
     <div>
       <h1>Hello World</h1>
+      <Overview />
       <QuestionList/>
     </div>
   );
