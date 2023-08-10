98bbc97cd5255643654bc213c49d49771d18d7a8
luckynumberthirteen <jtobono@gmail.com>
2023-08-10 14:54:17 -0700

    Imported styled-components

---
+++ client/src/components/Overview/Overview.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Text = styled.div`color: red;`
+
+const Overview = () => {
+
+  return(
+    <Text>Overview Image</Text>
+  )
+}
+
+export { Overview };
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-
+import { Overview } from './components/Overview/Overview.jsx';
 const App = () => {
 
   return (
     <div>
       <h1>Hello World</h1>
+      <Overview />
     </div>
   );
 }
