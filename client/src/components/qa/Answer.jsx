28710b166552b020a547c74b1a062ccc79379e3f
Frank Soto <fasfranksoto@gmail.com>
2023-08-11 16:24:37 -0700

    added username and date answered

--- client/src/components/qa/Answer.jsx
+++ client/src/components/qa/Answer.jsx
@@ -6,7 +6,11 @@ import React from 'react';
 const Answer = ({answer}) => (
 
   <div>
+    <div>
     A: {answer.body}
+    </div>
+    <div className="username"><b>{answer.answerer_name}</b></div>
+    <small className="data-answered">{answer.date}</small>
   </div>
 )
 
