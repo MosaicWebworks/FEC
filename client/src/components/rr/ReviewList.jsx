c14f2374ed230b7ef6e009194bba0e7ec9b6d246
“Paula <weigeyang1007@gmail.com>
2023-08-11 17:07:09 -0700

    see more review button works!

--- client/src/components/rr/ReviewList.jsx
+++ client/src/components/rr/ReviewList.jsx
@@ -2,36 +2,42 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useReviews} from './ReviewsContext.jsx';
 import { ReviewsProvider } from './ReviewsContext.jsx';
+//import { handleLoadMoreReviews } from './ReviewsContext.jsx';
 import ReviewTile from './ReviewTile.jsx';
 import exampleDataList from './exampleDataList';
 
 const Container = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: flex-start;
-border: solid black;
-width: 100%;
-height: 100vh;
+border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
 `;
 
+const Button = styled.button`
+`
+;
 
 const ReviewList = () => {
-  const { reviews, loadedReviewsCount } = useReviews();
+  const { reviews, loadedReviewsCount, handleLoadMoreReviews } = useReviews();
 
-  // more review button
-  const handleLoadMoreReviews = () => {
-    // Increase the loadedReviewsCount to load more reviews
-  };
+  // // more review button
+  // const handleLoadMoreReviews = () => {
+  //   // Increase the loadedReviewsCount to load more reviews
+  //   setLoadedReviewsCount(prevCount => prevCount + 2);
+  // };
 
+// console.log("loadedReviewsCount:", loadedReviewsCount);
+// console.log("reviews.length:", reviews.length);
   return (
     <Container>
-      {exampleDataList.results.map((review) => (
+      {exampleDataList.results.slice(0, loadedReviewsCount).map((review) => (
         <ReviewTile key={review.review_id} review={review} />
       ))}
-      {loadedReviewsCount < reviews.length && (
-        <button onClick={handleLoadMoreReviews}>
-        See More Reviews
-      </button>
+      {loadedReviewsCount < exampleDataList.results.length && (
+        <Button onClick={handleLoadMoreReviews}>
+          See More Reviews
+        </Button>
       )}
     </Container>
   );
--- client/src/components/rr/ReviewsContext.jsx
+++ client/src/components/rr/ReviewsContext.jsx
@@ -1,16 +1,19 @@
-
 import React, { createContext, useContext, useState } from 'react';
+import exampleDataList from './exampleDataList';
 
 const ReviewsContext = createContext();
 
 export const ReviewsProvider = ({ children }) => {
-  const [reviews, setReviews] = useState([]);
-  const [loadedReviewsCount, setLoadedReviewsCount] = useState(2);
+  const [reviews, setReviews] = useState(exampleDataList.results);
+  const [loadedReviewsCount, setLoadedReviewsCount] = useState(2); // Initial value is 2
 
-  // Load more reviews function
+  const handleLoadMoreReviews = () => {
+    // Increment the loadedReviewsCount by 2 each time the button is clicked
+    setLoadedReviewsCount(prevCount => prevCount + 2);
+  };
 
   return (
-    <ReviewsContext.Provider value={{ reviews, loadedReviewsCount }}>
+    <ReviewsContext.Provider value={{ reviews, loadedReviewsCount, handleLoadMoreReviews }}>
       {children}
     </ReviewsContext.Provider>
   );
