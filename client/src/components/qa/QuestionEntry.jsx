3f86c8190b3e3b00f6434846b60e514ef0ab092e
Frank Soto <fasfranksoto@gmail.com>
2023-08-14 12:35:31 -0700

    refactored code to render all questions(not just 1)

--- client/src/components/qa/QuestionEntry.jsx
+++ client/src/components/qa/QuestionEntry.jsx
@@ -1,9 +1,7 @@
-
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import exampleData from './exampleData.js'
 import Answer from './Answer.jsx'
-
 const Border = styled.div`
   display: grid;
   grid-template-areas: "question"
@@ -24,6 +22,7 @@ display: flex;
 flex-direction: column;
 overflow-y: auto;`
 
+
 const Button = styled.form`
 grid-area: button;
 display: flex;
@@ -34,25 +33,40 @@ justify-content: flex-end;`
 
 
 
-const QuestionEntry = () => {
 
-  const [answersID, setAnswersID] = useState(Object.keys(exampleData.results[0].answers))
+
+
+
+
+
+const QuestionEntry = ({qaObject}) => {
+  //converts answer id object into array. will help with determining how many answers exist
+  const [answersID, setAnswersID] = useState(Object.keys(qaObject.answers))
+  //answers that will be rendered. initially empty
   const [answersToRender, setAnswersToRender] = useState([]);
+  //how many answers will be rendered
   const [numberToRender, setNumberToRender] = useState(2);
 
-  //creates an array of object
-  let answers = Object.entries(exampleData.results[0].answers);
+
+  //creates an array of object. contains an array of arrays [0] = id and [1] = answer
+  let answers = Object.entries(qaObject.answers);
+
 
   //sorting function to sort by helpfulness with most helpful being at the top/front
   const compareHelpfulness = (a, b) => {
     return b[1].helpfulness - a[1].helpfulness;
   }
+
+
+  //calls sort function on array while sorting
   answers.sort(compareHelpfulness);
+
 
   //intiailly renders 2 answers for the question
   useEffect(() => {
     setAnswersToRender(answers.slice(0, numberToRender));
   }, [numberToRender])
+
 
   //handle submit of clicking on see more answers
   const handleSubmit = (e) => {
@@ -60,6 +74,7 @@ const QuestionEntry = () => {
     setNumberToRender(numberToRender + 2);
     console.log('number to render: ', numberToRender)
   }
+
 
   const displayButton = () => {
     if (numberToRender < answersID.length) {
@@ -75,30 +90,25 @@ const QuestionEntry = () => {
   return (
     <>
     <Border>
-      <Question><b>Q:</b> {exampleData.results[0].question_body}</Question>
+      <Question><b>Q:</b> {qaObject.question_body}</Question>
       <AnswerDesign>
         {
-          answersArray.map((answer) => (
-            <Answer answer={answer}/>
+          answersToRender.map((answer,index) => (
+            <Answer answer={answer[1]} key={answersID[index]}/>
           ))
         }
       </AnswerDesign>
       <form onSubmit={handleSubmit}>
-=========
-      <Answers>
-        {
-          answerArray.map((answer) => (
-            <Answer answer={answer} />
-          ))
-        }
-      </Answers>
-      <Button>
->>>>>>>>> Temporary merge branch 2
-          <input type="submit" value="see more answers" />
+          {displayButton()}
       </form>
+
+
+
+
     </Border>
     </>
   )
 }
+
 
 export default QuestionEntry;
--- client/src/components/qa/QuestionList.jsx
+++ client/src/components/qa/QuestionList.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import QuestionEntry from './QuestionEntry.jsx'
+import exampleData from './exampleData.js'
 
 
 
-const QuestionList = () => (
-  <div>
-    <QuestionEntry/>
-    <QuestionEntry/>
-  </div>
+const QuestionList = () => {
+  //contains an array of question objects
+  const [questions, setQuestions] = useState(Object.entries(exampleData.results));
 
-)
+  return (
+    <div>
+      {questions.map((question) => (
+        <QuestionEntry qaObject={question[1]} key={question[0]}/>
+      ))}
+
+    </div>
+  )
+
+}
 
 
 export default QuestionList;
