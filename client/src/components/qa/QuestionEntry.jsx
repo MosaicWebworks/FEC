e424e3a3bb74a9451433734f0660551339d85b08
Frank Soto <fasfranksoto@gmail.com>
2023-08-14 12:17:42 -0700

    refactored to take object passed in

--- client/src/components/qa/QuestionEntry.jsx
+++ client/src/components/qa/QuestionEntry.jsx
@@ -31,18 +31,23 @@ justify-content: flex-end;`
 
 
 
-const QuestionEntry = () => {
-  const [answersID, setAnswersID] = useState(Object.keys(exampleData.results[0].answers))
+const QuestionEntry = ({qaObject}) => {
+  //converts answer id object into array. will help with determining how many answers exist
+  const [answersID, setAnswersID] = useState(Object.keys(qaObject.answers))
+  //answers that will be rendered. initially empty
   const [answersToRender, setAnswersToRender] = useState([]);
+  //how many answers will be rendered
   const [numberToRender, setNumberToRender] = useState(2);
 
-  //creates an array of object
-  let answers = Object.entries(exampleData.results[0].answers);
+  //creates an array of object. contains an array of arrays [0] = id and [1] = answer
+  let answers = Object.entries(qaObject.answers);
 
   //sorting function to sort by helpfulness with most helpful being at the top/front
-  const compareHelpfulness = (a,b) => {
+  const compareHelpfulness = (a, b) => {
     return b[1].helpfulness - a[1].helpfulness;
   }
+
+  //calls sort function on array while sorting
   answers.sort(compareHelpfulness);
 
   //intiailly renders 2 answers for the question
@@ -70,7 +75,7 @@ const QuestionEntry = () => {
   return (
     <>
     <Border>
-      <Question><b>Q:</b> {exampleData.results[0].question_body}</Question>
+      <Question><b>Q:</b> {qaObject.question_body}</Question>
       <AnswerDesign>
         {
           answersToRender.map((answer,index) => (
