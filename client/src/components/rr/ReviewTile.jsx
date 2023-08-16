a952f292f37238d1d48c10280c7836f427a384fe
“Paula <weigeyang1007@gmail.com>
2023-08-16 10:21:16 -0700

    first try on the quarter stars

--- client/src/components/rr/ReviewTile.jsx
+++ client/src/components/rr/ReviewTile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import exampleDataList from './exampleDataList.js';
+//import exampleDataList from './exampleDataList.js';
 //import StarRating from './StarRating.jsx';
 
 const Tile = styled.div`
@@ -19,25 +19,58 @@ const Star = styled.span`
   color: ${({ isActive }) => (isActive ? 'gold' : 'gray')};
 `;
 
+const FullStar = styled(Star)`
+  color: gold;
+`;
+
+const HalfStar = styled(Star)`
+  position: relative;
+  color: gray;
+
+  &::before {
+    content: '\2605';
+    position: absolute;
+    width: 50%;
+    overflow: hidden;
+    color: gold;
+    z-index: 1;
+    left: 0;
+    top: 0;
+  }
+`;
+
+
 const StarRating = ({ rating, totalStars }) => {
-  return (
-    <StarContainer>
-      {[...Array(totalStars)].map((star, index) => {
-        const isActive = index < Math.floor(rating);
+  const fullStars = Math.floor(rating);
+  const decimalPart = rating - fullStars;
 
-        return (
-          <Star
-            key={index}
-            isActive={isActive}
-          >
-            &#9733;
-          </Star>
-        );
-      })}
-    </StarContainer>
-  );
+   //console.log('rating is: ', rating);
+  //  console.log(fullStar);
+  //  console.log(decimalPart);
+
+  const renderStar = (index) => {
+
+    console.log('index:', index, 'fullStars:', fullStars, 'decimalPart:', decimalPart);
+
+    if (index < fullStars) {
+      return <FullStar key={index} isActive>&#9733;</FullStar>;
+    } else if (index === fullStars) {
+      if (decimalPart >= 0.75) {
+        return <FullStar key={index} isActive>&#9733;</FullStar>;
+      } else if (decimalPart >= 0.25) {
+        return <HalfStar key={index} isActive>&#9733;</HalfStar>;
+      } else {
+        return <Star key={index}>&#9734;</Star>;
+      }
+    } else {
+      return <Star key={index}>&#9734;</Star>;
+    }
+
+
+  };
+
+  return <StarContainer>{[...Array(totalStars)].map((star, index) => renderStar(index))}</StarContainer>;
 };
-
 
 const Summary = styled.div`
   font-weight: bold;
