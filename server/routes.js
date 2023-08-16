75f2394377e79242c10096d550a575a9cd111078
luckynumberthirteen <jtobono@gmail.com>
2023-08-16 16:41:59 -0700

    Added support for GET  reviews

--- server/routes.js
+++ server/routes.js
@@ -4,14 +4,15 @@ const axios = require("axios");
 
 router.get("/products*", function (req, res)
 {
-  //split the url of the req to get whichever url the client needs
   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products${req.url.split('/products')[1]}`, {headers: {Authorization: `${process.env.REACT_APP_API_KEY}`}})
   .then((data) => res.send(data.data));
 });
 
-router.get("/reviews", function (req, res)
+router.get("/reviews*", function (req, res)
 {
-  //do stuff
-})
+  //split the url of the req to get whichever url the client needs
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews${req.url.split('/reviews')[1]}`, {headers: {Authorization: `${process.env.REACT_APP_API_KEY}`}})
+  .then((data) => res.send(data.data));
+});
 
 module.exports = router;
