51bc08dee6dda64e19dae6f511473b47a474df71
luckynumberthirteen <jtobono@gmail.com>
2023-08-16 11:03:53 -0700

    Bare minimum server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.listen(process.env.PORT);
+console.log(`Listening at http://localhost:${process.env.PORT}`);
