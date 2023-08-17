be7b16b7cc9e0890e4f05c6c7cf54320f4a85f41
Frank Soto <fasfranksoto@gmail.com>
2023-08-17 12:34:12 -0700

    completed refactor of code to use axios

--- client/src/components/qa/QuestionList.jsx
+++ client/src/components/qa/QuestionList.jsx
@@ -1,24 +1,44 @@
 import React, {useState, useEffect} from 'react';
 import QuestionEntry from './QuestionEntry.jsx'
-import exampleData from './exampleData.js'
+import exampleData from './exampleData.js';
+import styled from 'styled-components';
+import axios from 'axios';
 
+
+const MaxHeight = styled.div`
+max-height: 500px;
+overflow-y: auto`
 
 
 const QuestionList = ({productID}) => {
-  //contains an array of question objects
-  const [questionsObject, setQuestionsObject] = useState(Object.entries(exampleData.results));
+  //contains an array of question objects  //Object.entries(exampleData.results)
+  const [questionsObject, setQuestionsObject] = useState([]);
   //determines how many questions to render
   const [toRender, setToRender] = useState(2);
   //contains the questions that will be rendered
   const [questionsToRender, setQuestionsToRender] = useState([]);
 
-  //initially renders the questions and re-renders upon button click for more questions
+  //makes initial api call and stores fetched info into different states
+    //can refactor to use one less state?
   useEffect(() => {
-    setQuestionsToRender(questionsObject.slice(0, toRender));
-  }, [toRender])
+    axios.get(`data/qa/questions/?product_id=40340`)
+      .then((results) => {
+        setQuestionsObject(Object.entries(results.data.results))
+        setQuestionsToRender(Object.entries(results.data.results).slice(0, toRender));
+      })
+      .catch((err) => console.log(err));
+    }, [])
+
+
+    ////initially renders the questions and re-renders upon button click for more questions
+    useEffect(() => {
+      setQuestionsToRender(questionsObject.slice(0, toRender));
+    }, [toRender])
+
+
 
   //will display more buttons when clicked on. Will disappear if no more questions available
-  const moreQuestions = (e) => {
+  const renderMoreQuestions = (e) => {
 
     if (toRender < questionsObject.length) {
       return (
@@ -35,13 +55,15 @@ const QuestionList = ({productID}) => {
   }
   return (
     <div>
-      {questionsToRender.map((question) => (
-        <QuestionEntry qaObject={question[1]} key={question[0]}/>
-      ))}
-    <form onSubmit={handleSubmit}>
-        {moreQuestions()}
-    </form>
-
+        <MaxHeight>
+        {questionsToRender && questionsToRender.map((question) => (
+            <QuestionEntry qaObject={question[1]} key={question[0]}/>
+          ))
+        }
+        </MaxHeight>
+      <form onSubmit={handleSubmit}>
+          {renderMoreQuestions()}
+      </form>
     </div>
   )
 
