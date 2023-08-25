5be43f1e1fd1d6fb26d247eb1887d380c6b0b4de
luckynumberthirteen <jtobono@gmail.com>
2023-08-25 09:50:30 -0700

    Used styled buttons

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -47,12 +47,25 @@ const AddToCartLayout = styled.div`
   row-gap: 15px;
 `
 
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.background};
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  transition: 0.3s;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.textContrast};
+  }
+`;
+
+
 const AddToBag = () => {
-  return(<button>Add to Cart</button>);
+  return(<Button>Add to Cart</Button>);
 }
 
 const Favorite = () => {
-  return(<button>Favorite</button>);
+  return(<Button>Favorite</Button>);
 }
 
 const AddToCart =
